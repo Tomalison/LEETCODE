6706333052d83f85b375a1c2e74c608997442991
蘇文呈 <tomsu25478@gmail.com>
2023-04-08 11:44:16 +0800

    練習

---
+++ 第一題_數字疊加.py
@@ -0,0 +1,10 @@
+#Leetcode第一題，Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).Return the running sum of nums.
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
+nums = [1,2,3,4]
+print(Solution().runningSum(nums))
