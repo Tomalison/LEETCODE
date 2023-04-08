3530a03850a377de8392d39de3af653646c0f45b
tomsu <tom.su@pet100pa.com>
2023-04-08 17:02:40 +0800

    提交第六題到github上

---
+++ 第六題_字串購建對照(Ransom Note).py
@@ -0,0 +1,12 @@
+'''Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.'''
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for i in ransomNote:
+            if i in magazine:
+                magazine = magazine.replace(i, '', 1)
+            else:
+                return False
+        return True
